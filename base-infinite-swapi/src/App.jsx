e0523b04b23ca7f6ddf88176fb03b525c7628af5
Beanxx <tnqls991211@naver.com>
2023-02-15 11:32:45 +0900

    feat: useInfiniteQuery 호출

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,14 +1,21 @@
 import "./App.css";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools"; // 노드 환경을 변경할 수 있을 때만 보이게 됨(dev mode)
+
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
-    </div>
+    <QueryClientProvider client={QueryClient}>
+      <div className="App">
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        {/* <InfiniteSpecies /> */}
+        <ReactQueryDevtools />
+      </div>
+    </QueryClientProvider>
   );
 }
 
